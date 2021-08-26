fa10d707defb60f77a1f4e0ed616a7bebbca511b
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-26 14:59:27 -0300

    Update Exer11.py

--- Exer11.py
+++ Exer11.py
@@ -1,2 +1,2 @@
-x = int(input("Insira o preço do prduto: "))
+x = float(input("Insira o preço do prduto: "))
 print ("Com o reajuste, o valor do produto foi de R${} para R${:.2f}".format(x,(x*0.95)))
