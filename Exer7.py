75fcabc3aba1535f92c2d3956798b85d21ceb155
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-26 14:59:18 -0300

    Update Exer7.py

--- Exer7.py
+++ Exer7.py
@@ -1,4 +1,4 @@
-n1 = int(input("Insira a primeira nota: "))
-n2 = int(input("Insira a segunda nota: "))
+n1 = float(input("Insira a primeira nota: "))
+n2 = float(input("Insira a segunda nota: "))
 n3 = ((n1+n2)/2)
 print(n3)
