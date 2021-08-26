eea7dfe26ce39712cd2e31201a5cf694b08bc5db
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-26 14:59:21 -0300

    Update Exer9.py

--- Exer9.py
+++ Exer9.py
@@ -1,3 +1,3 @@
-x = int(input("Insira um valor em metros: "))
+x = float(input("Insira um valor em metros: "))
 print("Valor em centímetros: " , (x*100))
 print(("Valor em milímetros: ") , (x*1000))
